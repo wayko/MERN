10de052d674976fc5408688aa6c401bdf4524faa
Wayko <programmers@dreamcpu.com>
2020-01-13 16:59:35 -0500

    Added connection to MongoDB

---
+++ utils/connectDb.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+const connection = {}
+
+async function connectDb()
+{
+	if(connection.isConnected)
+		{
+			//Use existing db connection
+			console.log("Using existing connection")
+			return;
+		}
+	// Use new database connection
+	const db = await mongoose.connect(process.env.MONGO_SRV,  {
+		
+		useCreatIndex: true,
+		useFindAndModify: false,
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	
+	console.log("DB Connected")
+	connection.isConnected = db.connections[0].readyState;
+	
+}
+
+export default connectDb;
