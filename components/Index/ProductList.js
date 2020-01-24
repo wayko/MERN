84605e89779980142c65303b9fe39e1c2f18f073
Wayko <programmers@dreamcpu.com>
2020-01-24 17:59:45 -0500

    Added responsivness and products

--- components/Index/ProductList.js
+++ components/Index/ProductList.js
@@ -1,5 +1,21 @@
-function ProductList() {
-  return <>ProductList</>;
+import { Card } from 'semantic-ui-react'
+
+function ProductList({ products }) {
+	
+  function mapProductsToItems(products)
+  {
+	  return products.map(product =>
+	  ({
+		 header: product.name,
+		 image: product.mediaUrl,
+		 meta: `$${product.price}`,
+		 color: 'blue',
+		 fluid: true,
+		 childKey: product._id,
+		 href: `/product?_id=${product._id}`
+	  }));
+  }
+  return <Card.Group stackable itemsPerRow="4" centered items={mapProductsToItems(products)}/>;
 }
 
 export default ProductList;
